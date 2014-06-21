e9f034b4f47bbc7a5edb68b6fab31266d080aec7
derekgilwa <dgliwa@thoughtworks.com>
2014-06-21 14:43:19 -0500

    init + initial empty board Test and print statement

---
+++ src/com/thoughtworks/tictactoe2/Main.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.tictactoe2;
+
+/**
+ * Created by derekgilwa on 6/21/14.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game(System.out);
+
+    }
+}
